5c430cea9ee3fef4ac85ab3ef3ee15f2acbc4052
Hiroyuki Iio <crchondrite@gmail.com>
2017-11-19 21:37:49 +0900

    use cache

--- src/main/kotlin/repository/BookRepository.kt
+++ src/main/kotlin/repository/BookRepository.kt
@@ -10,4 +10,6 @@ object BookRepository {
         Book("9784839961749", "Kotlin in Action", 4114, Author("1", "foo")),
         Book("9784839961741", "Spark  in Action", 2110, Author("2", "bar"))
     )
+
+    fun testCacheBook(isbn: String) : Book = Book(isbn + System.currentTimeMillis(), "Kotlin in Action", 4114, Author("1", "foo"))
 }
