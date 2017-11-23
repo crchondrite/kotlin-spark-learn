0e79c750e3b0991de837c54feb4fd5b7ef72d955
Hiroyuki Iio <crchondrite@gmail.com>
2017-11-23 11:17:32 +0900

    list parameter

--- src/main/kotlin/entity/Items.kt
+++ src/main/kotlin/entity/Items.kt
@@ -4,10 +4,20 @@ data class Items(
         var _id: String,
         var name: String,
         var value: Int,
-        var enabled: Boolean
+        var enabled: Boolean,
+        var variations: List<Variation>
 ) {
     constructor(): this(
-            "", "", 0, false
+            "", "", 0, false, listOf()
+    )
+}
+
+data class Variation(
+        var id: Int,
+        var value:String
+) {
+    constructor(): this(
+            0, ""
     )
 }
 
