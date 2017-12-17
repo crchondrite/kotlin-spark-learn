06545efe068ff5ac3aefbe84b7cd37e3113b9e12
Hiroyuki Iio <crchondrite@gmail.com>
2017-12-17 19:45:39 +0900

    commit

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-import Bulma from 'bulma'
-
 let $ = require("jquery");
 
 $(function () {
   var myAlert = function () {
-    console.log("alert");
+    console.log("hoge");
   }
 });
