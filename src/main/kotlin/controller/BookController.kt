0192226784fe6200db4450651747dff5cc3c2439
Hiroyuki Iio <crchondrite@gmail.com>
2017-12-17 13:42:56 +0900

    set template engine

--- src/main/kotlin/controller/BookController.kt
+++ src/main/kotlin/controller/BookController.kt
@@ -1,10 +1,17 @@
 package controller
 
+import controller.MytemplateEngine.TEMPLATE_ENGINE
+import entity.Book
 import service.BookService
+import spark.ModelAndView
 import spark.Route
 
 object BookController {
     fun findAll() = Route { _, _ ->
-        JsonTransformer.render(BookService.findAll())
+        TEMPLATE_ENGINE.render(
+            ModelAndView(
+                mapOf("books" to BookService.findAll()), "index"
+            )
+        )
     }
 }
